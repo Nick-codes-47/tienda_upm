8a07573d36e07664bc15964540d229d184df8f8d
Nico <nicolaslefterache@gmail.com>
2025-09-29 20:00:53 +0200

    Hecho el constructor de la clase Product (se me olvidó)

--- src/main/java/es/upm/etsisi/poo/Product.java
+++ src/main/java/es/upm/etsisi/poo/Product.java
@@ -9,6 +9,20 @@ public class Product {
     private String name;
     private int price;
 
+    /**
+     * This constructor is done to introduce all the attributes the user wants for the product
+     * @param category The category of the product
+     * @param id The id of the product
+     * @param name The name of the product
+     * @param price The price of the product
+     */
+    public Product(Category category, int id, String name, int price) {
+        this.category = category;
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
     public int getId() { return this.id; }
     public void setId(int id) { this.id = id; }
     public String getName() { return this.name; }
