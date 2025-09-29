f6e6979f809e98cab923e555fb04a3b0f6613fbe
Nico <nicolaslefterache@gmail.com>
2025-09-29 19:15:30 +0200

    Creado enum Category y getters y setters Product

---
+++ src/main/java/es/upm/etsisi/poo/Category.java
@@ -0,0 +1,5 @@
+package es.upm.etsisi.poo;
+
+public enum Category {
+    MERCH, PAPELERIA, ROPA, LIBRO, ELECTRONICA
+}
--- src/main/java/es/upm/etsisi/poo/Product.java
+++ src/main/java/es/upm/etsisi/poo/Product.java
@@ -1,4 +1,16 @@
 package es.upm.etsisi.poo;
 
 public class Product {
+    private Category category;
+    private int id;
+    private String name;
+    private int price;
+
+    public int getId() { return this.id; }
+    public String getName() { return this.name; }
+    public void setName(String name) { this.name = name; }
+    public int getPrice() { return this.price; }
+    public void setPrice(int price) { this.price = price; }
+    public Category getCategory() { return this.category; }
+    public void setCategory(Category category) { this.category = category; }
 }
