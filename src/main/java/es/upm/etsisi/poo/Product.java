38c882cb594bc0d975ea357f4fc76cb6bfe1e500
Nico <nicolaslefterache@gmail.com>
2025-09-29 19:22:33 +0200

    Hecho método equals

--- src/main/java/es/upm/etsisi/poo/Product.java
+++ src/main/java/es/upm/etsisi/poo/Product.java
@@ -7,10 +7,19 @@ public class Product {
     private int price;
 
     public int getId() { return this.id; }
+    public void setId(int id) { this.id = id; }
     public String getName() { return this.name; }
     public void setName(String name) { this.name = name; }
     public int getPrice() { return this.price; }
     public void setPrice(int price) { this.price = price; }
     public Category getCategory() { return this.category; }
     public void setCategory(Category category) { this.category = category; }
+
+    /**
+     * Compares this product to another with the ID
+     * @param product (another)
+     * @return whether the IDs match
+     */
+    public boolean equals(Product product) { return this.id == product.id; }
+
 }
