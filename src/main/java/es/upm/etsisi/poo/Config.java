4bea7d2fb13adcb7676ae0919c183646ffc18cd9
Miguel Ángel Muñoz <miguelmunozfdez@gmail.com>
2025-09-30 17:31:52 +0200

    Completed VariableLoader for Config

--- src/main/java/es/upm/etsisi/poo/Config.java
+++ src/main/java/es/upm/etsisi/poo/Config.java
@@ -2,14 +2,17 @@ package es.upm.etsisi.poo;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 public class Config {
     private static final String CONFIG_DEFAULT_FILENAME = "config.txt";
 
-    private String configFileName;
+    private final String configFileName;
 
     private Map<String, Double> categories;
 
@@ -27,39 +30,6 @@ public class Config {
 
         System.err.printf("LOG::Config> Loading config from file %s\n", configFileName);
         loadConfigFile(configFileName);
-    }
-
-    private void loadConfigFile(String configFilePath)
-    {
-        try (Scanner scanner = new Scanner(new File(configFilePath)))
-        {
-            System.err.printf("LOG::Config> Loading application variables...\n");
-            loadVariables(scanner);
-            System.err.printf("LOG::Config> Loading categories and its discounts ...\n");
-
-        }
-        catch (FileNotFoundException exception)
-        {
-            System.out.printf("Configuration load failure: missing %s file in %s\n", configFilePath);
-        }
-    }
-
-    public void loadVariables(Scanner scanner)
-    {
-        Map<String, Consumer> variables = Map.of(
-                "MaxProducts", Config::setMaxProducts
-        );
-
-        for (String variable: variables.keySet())
-        {
-            System.err.printf("LOG::Config> Getting %s value ...\n", variable);
-        }
-
-        setMaxProducts(Integer.parseInt(scanner.nextLine()));
-        System.err.printf("LOG::Config> MaxProducts set with value %d\n", maxProducts);
-        System.err.printf("LOG::Config> Getting MaxProductPerTicket value ...\n");
-        setMaxProductPerTicket(Integer.parseInt(scanner.nextLine()));
-        System.err.printf("LOG::Config> MaxProductPerTicket set with value %d\n", maxProductPerTicket);
     }
 
     public boolean validCategory(String category)
@@ -94,8 +64,125 @@ public class Config {
         return maxProductPerTicket;
     }
 
-    private int setMaxProductPerTicket(int value)
+    private void setMaxProductPerTicket(int value)
     {
         maxProductPerTicket = value;
+    }
+
+    private void loadConfigFile(String configFilePath)
+    {
+        try (Scanner scanner = new Scanner(new File(configFilePath)))
+        {
+            VariableLoader variableLoader = new VariableLoader(this);
+
+            System.err.printf("LOG::Config> Loading application variables...\n");
+            variableLoader.loadVariables(scanner);
+            System.err.printf("LOG::Config> Loading categories and its discounts ...\n");
+//            loadCategories(scanner);
+        }
+        catch (FileNotFoundException exception)
+        {
+            System.out.printf("Configuration load failure: missing %s file in %s\n", configFilePath);
+        }
+
+    }
+
+    private class VariableLoader {
+        private final Config config;
+        public Map<String, VariableEntry> variables = new HashMap<>();
+
+        VariableLoader(Config config)
+        {
+            this.config = config;
+            initVariableEntries();
+        }
+
+        private void initVariableEntries()
+        {
+            variables.put(
+                    "MaxProducts",
+                    new VariableEntry(
+                            "MaxProducts",
+                            s -> config.setMaxProducts(Integer.parseInt(s)),
+                            () -> String.valueOf(config.getMaxProducts())
+                    )
+            );
+            variables.put(
+                    "MaxProductsPerTicket",
+                    new VariableEntry(
+                            "MaxProductsPerTicket",
+                            s -> config.setMaxProductPerTicket(Integer.parseInt(s)),
+                            () -> String.valueOf(config.getMaxProductPerTicket())
+                    )
+            );
+        }
+
+        public void loadVariables(Scanner scanner)
+        {
+            try
+            {
+                for (int i = 0; i < variables.size(); i++)
+                {
+                    loadVariable(scanner.nextLine());
+                }
+            }
+            catch (NoSuchElementException exception)
+            {
+                System.err.printf("ERROR::Config::VariableLoader> Missing variables in config file\n");
+            }
+            catch (IndexOutOfBoundsException exception)
+            {
+                System.err.printf("ERROR::Config::VariableLoader> Syntax error in config file: expecting '=' after variable name\n");
+            }
+
+            checkLoadedVariables(variables);
+        }
+
+        private void loadVariable(String line)
+        {
+            String key = getVariableKey(line);
+            String value = getVariableValue(line);
+            VariableEntry variable = variables.get(key);
+            variable.setter.accept(value);
+
+            System.err.printf("LOG::Config::VariableLoader> %s = %s", key, variable.getter.get());
+        }
+
+        private void checkLoadedVariables(Map<String, VariableEntry> variables)
+        {
+            for (VariableEntry variable : variables.values())
+            {
+                if (!variable.loaded)
+                {
+                    throw new RuntimeException(
+                            String.format("Missing variable %s\n", variable.name.toUpperCase())
+                    );
+                }
+            }
+        }
+
+        private String getVariableKey(String line)
+        {
+            return line.substring(0, line.indexOf('='));
+        }
+
+        private String getVariableValue(String line)
+        {
+            return line.substring(line.indexOf('=') + 1);
+        }
+
+        private class VariableEntry {
+            public String name;
+            public Consumer<String> setter;
+            public Supplier<String> getter;
+            boolean loaded = false;
+
+            VariableEntry(String name, Consumer<String> setter, Supplier<String> getter)
+            {
+                this.name = name;
+                this.setter = setter;
+                this.getter = getter;
+            }
+        }
     }
 }
