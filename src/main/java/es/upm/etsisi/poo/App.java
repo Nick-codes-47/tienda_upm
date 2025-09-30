dc7eed27cdd690e8931238303d2aae347d252a69
Nico <nicolaslefterache@gmail.com>
2025-09-30 10:41:05 +0200

    empezado updateProduct(), falta retocar alguna cosa

--- src/main/java/es/upm/etsisi/poo/App.java
+++ src/main/java/es/upm/etsisi/poo/App.java
@@ -1,11 +1,12 @@
 package es.upm.etsisi.poo;
 
-import java.util.ArrayList;
+import java.lang.reflect.Field;
+import java.util.HashMap;
 
 public class App
 {
     Ticket currentTicket;
-    ArrayList<Product> productList;
+    HashMap<Integer, Product> products;
     Config config;
 
     public static void main( String[] args ) {
@@ -17,8 +18,34 @@ public class App
 
     public void printProdList() {
         System.out.println("Catalog: ");
-        for (Product p : this.productList) {
-            System.out.println(" "+p.toString());
+        for (Product product : products.values()) {
+            System.out.println(" "+product.toString());
+        }
+    }
+
+    /**
+     * Method to update the fields of a product (NAME, CATEGORY, PRICE) in the catalog
+     * @param id to search the product
+     * @param field to know which field of the product needs to be changed
+     * @param value the vnew value of the field
+     * @return -1 if the product doesn't exist in the catalog.
+     *          1 if the product exists but the field doesn't
+     *          0 if the field was changed correctly
+     */
+    public int updateProduct(int id, String field, String value) {
+        Product product = products.get(id);
+        if (product == null) { return -1; }
+        else {
+            // We try to get the product's field to be modified
+            try {
+                Field f = Product.class.getDeclaredField(field);
+                f.setAccessible(true);
+                f.set(product, value);
+            } catch (NoSuchFieldException e) {
+                return 1; // if field doesn't exist we return 1
+            } catch (IllegalAccessException e) {
+                return 2; //
+            }
         }
     }
 }
