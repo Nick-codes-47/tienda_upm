699b8df6ad646a38a6d2490ac3be841ed41f119b
Alvaro <a.marquez.santamaria@gmail.com>
2025-09-30 10:41:41 +0200

    Ticket toString, addProduct

--- src/main/java/es/upm/etsisi/poo/Ticket.java
+++ src/main/java/es/upm/etsisi/poo/Ticket.java
@@ -5,28 +5,39 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Ticket {
-    HashMap<Product, Integer> ticket;
+    private HashMap<Product, Integer> ticket;
+    private HashMap<String, Integer> categories;
 
-    Ticket (Config config) {
+    public Ticket (Config config) {
         this.ticket = new HashMap<>();
+        this.categories = new HashMap<>();
+
     }
 
+    /**
+     * Method to add products to ticket
+     * @param product
+     * @param amount
+     * @return -1 if product is not added, 0 if product is added
+     */
     public int addProduct (Product product, int amount){
-        if (ticket.size() >= config.getNumMaxElementos()){ // Ticket supera el limite de elementos
+        if (ticket.size() >= config.getNumMaxElementos()){
+            // Product cannot be added due to override
             System.err.println("Too many products in ticket");
             return -1;
         }
 
-        else { // El producto puede introducirse
+        else {
+            // Product can be added
             ticket.put(product, amount);
             System.out.println(ticket.toString());
-
 
             System.out.println("Ticket add: ok");
 
         }
         return -1;
     }
+
     public int deleteProdouct (int id){
         return -1;
     }
@@ -36,26 +47,33 @@ public class Ticket {
     public double getPrice() {
         return -1;
     }
+
+
     public String toString() {
         String str = "";
         double totalPrice = 0;
         double totalDiscount = 0;
-        double finalPrice = 0;
 
         for (Map.Entry<Product, Integer> entry : ticket.entrySet()) {
             Product producto = entry.getKey();
             Integer cantidad = entry.getValue();
+
             str += producto.toString();
             totalPrice += producto.getPrice() * cantidad;
 
-            if (cantidad > 1){
-                str += "**discount";
-                totalDiscount += cantidad * config.getDiscount(producto.getCategory());
+            if (categories.get(producto.getCategory()) > 1) {
+                double discount = producto.getPrice() - (producto.getPrice() * config.getDiscount(producto.getCategory()));
+                str += "**discount -" + discount;
+                totalDiscount += discount;
             }
-        }
+
+            str += "\n";
+
+            }
+
         str += "\nTotal price: " + totalPrice;
         str += "\nTotal discount: " + totalDiscount;
-        str += "\nFinal price: " + finalPrice;
+        str += "\nFinal price: " + (totalPrice - totalDiscount);
 
         return str;
     }
