8938f7a410c47dab97f3d67d5e053d0076427e12
Nico <nicolaslefterache@gmail.com>
2025-11-04 18:13:03 +0100

    Creadas clases para las Actions de Tickets

---
+++ src/main/java/es/upm/etsisi/poo/TicketHandlerActions/ListTickets.java
@@ -0,0 +1,21 @@
+package es.upm.etsisi.poo.TicketHandlerActions;
+
+import es.upm.etsisi.poo.App;
+import es.upm.etsisi.poo.Action;
+
+public class ListTickets extends Action {
+    public ListTickets(App app) {
+        super(app);
+    }
+
+    @Override
+    public int execute(String[] args) {
+        // TODO ALL
+        return 0;
+    }
+
+    @Override
+    public void help() {
+        // TODO ALL
+    }
+}
