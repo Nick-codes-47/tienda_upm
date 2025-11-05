fc3a663179fab739cfdee5e4eb8d5f6fb55abf86
Nico <nicolaslefterache@gmail.com>
2025-11-05 12:47:01 +0100

    Creadas clases relacionadas con el contenedor de tickets

---
+++ src/main/java/es/upm/etsisi/poo/TicketEntry.java
@@ -0,0 +1,18 @@
+package es.upm.etsisi.poo;
+
+public class TicketEntry {
+    public String cashId;
+    public String customerId;
+    public Ticket ticket;
+
+    public TicketEntry(String cashId, String customerId, Ticket ticket) {
+        this.cashId = cashId;
+        this.customerId = customerId;
+        this.ticket = ticket;
+    }
+
+    public Ticket getTicket() {
+        return this.ticket;
+    }
+
+}
