c0d28b60961eee653371754ca725c6b068cbb838
Nico <nicolaslefterache@gmail.com>
2025-11-05 19:03:58 +0100

    comprobacion sobre maxPeople y expireDate

--- src/main/java/es/upm/etsisi/poo/ProductContainer/Event.java
+++ src/main/java/es/upm/etsisi/poo/ProductContainer/Event.java
@@ -7,8 +7,19 @@ public class Event extends BaseProduct {
     public Event(int id, String name, double price, LocalDate expireDate, int maxPeople, EventType type)
         throws InvalidProductException{
         super(id,name, price);
-        // TODO comprobaciones sobre expireDate y maxPeople
+        // TODO comprobaciones sobre planificaciones según el type
+        if (maxPeople < 1 || maxPeople > 100)
+            throw new InvalidProductException("ERROR: Events must have between 1 and 100 people");
+        if (expireDate == null || expireDate.isBefore(LocalDate.now()))
+            throw new InvalidProductException("ERROR: ExpireDate can't be null or before now");
+        this.expireDate = expireDate;
+        this.maxPeople = maxPeople;
+        this.type = type;
     }
+
+    public LocalDate getExpireDate() { return this.expireDate; }
+    public int getMaxPeople() { return this.maxPeople; }
+    public EventType getType() { return this.type; }
 
     private LocalDate expireDate;
     private int maxPeople;
