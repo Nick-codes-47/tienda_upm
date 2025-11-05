a60ae18a8f440d949b6c8853557d14fd2a68ba6a
Nico <nicolaslefterache@gmail.com>
2025-11-05 11:55:49 +0100

    Creadas clase RequestHandler

---
+++ src/main/java/es/upm/etsisi/poo/RequestHandler.java
@@ -0,0 +1,16 @@
+package es.upm.etsisi.poo;
+
+import java.util.HashMap;
+
+public class RequestHandler {
+    private HashMap<String, Action> actions;
+
+    public RequestHandler() {
+        actions = new HashMap<>();
+    }
+
+    public Action getAction(Request request) {
+        // TODO ALL
+        return null;
+    }
+}
