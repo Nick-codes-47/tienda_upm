61e2d25f63f62e713617ee5966fb79ab71de15a7
Nico <nicolaslefterache@gmail.com>
2025-11-05 11:39:33 +0100

    Creadas clases para las Actions de Users (podrían faltar cosas)

---
+++ src/main/java/es/upm/etsisi/poo/UserHandlerActions/ListUsers.java
@@ -0,0 +1,21 @@
+package es.upm.etsisi.poo.UserHandlerActions;
+
+import es.upm.etsisi.poo.Action;
+import es.upm.etsisi.poo.App;
+
+public class ListUsers extends Action {
+    public ListUsers(App app) {
+        super(app);
+    }
+
+    @Override
+    public int execute(String[] args) {
+        // TODO ALL
+        return 0;
+    }
+
+    @Override
+    public void help() {
+        // TODO ALL
+    }
+}
