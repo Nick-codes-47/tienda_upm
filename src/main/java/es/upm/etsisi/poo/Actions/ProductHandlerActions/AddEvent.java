5cab3bb1ec7e339fcd666de6e6222a744ef098fa
Nico <nicolaslefterache@gmail.com>
2025-11-09 14:52:09 +0100

    class finished (may require review in the future)

---
+++ src/main/java/es/upm/etsisi/poo/Actions/ProductHandlerActions/AddEvent.java
@@ -0,0 +1,87 @@
+package es.upm.etsisi.poo.Actions.ProductHandlerActions;
+
+import es.upm.etsisi.poo.Actions.Action;
+import es.upm.etsisi.poo.App;
+import es.upm.etsisi.poo.ProductContainer.BaseProduct;
+import es.upm.etsisi.poo.ProductContainer.Event;
+import es.upm.etsisi.poo.ProductContainer.EventType;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
+
+/**
+ * Class to add an Event to the Catalog
+ */
+public class AddEvent extends Action {
+    public AddEvent(App app) {
+        super(app);
+    }
+
+    /**
+     * Method that executes the action to add an event to the catalog
+     * @param args the arguments required to add an event to the catalog
+     * @return 0 if all went well
+     *         1 if an argument was invalid to parse
+     *         2 if one of the arguments was invalid to create the product
+     *         3 if they weren't enough arguments
+     */
+    @Override
+    public int execute(String[] args) {
+        if (args.length == 4) {
+            int newId = app.catalog.getNewId();
+            try {
+                BaseProduct event = new Event(
+                        newId,                           // ID
+                        args[0],                         // name
+                        Double.parseDouble(args[1]),     // price
+                        LocalDate.parse(args[2]),        // expiration date
+                        Integer.parseInt(args[3]),       // max people
+                        this.eventType                   // type
+                );
+                return app.catalog.add(event);
+            } catch (NumberFormatException e) {
+                System.err.println("ERROR: price and/or max_people are not valid");
+                return 1;
+            } catch (DateTimeParseException e) {
+                System.err.println("ERROR: the date MUST have the format: yyyy-MM-dd");
+                return 1;
+            } catch (BaseProduct.InvalidProductException e) {
+                System.err.println(e.getMessage());
+                return 2;
+            }
+        } else {
+            System.err.println("ERROR: wrong number of arguments");
+            return 3;
+        }
+    }
+
+    /**
+     * Shows how to call all actions that involve adding an event
+     */
+    @Override
+    public void help() {
+        for (EventType eventType : EventType.values()) {
+            System.out.println("add"+ toSentenceCase(eventType) +
+                    " \"<name>\" <price> <expiration: yyyy-MM-dd> <max_people>");
+        }
+    }
+
+    public void setEventType(EventType eventType) {
+        this.eventType = eventType;
+    }
+
+    private String toSentenceCase(EventType eventType) {
+        // Store in a String the eventType
+        String type = eventType.name();
+
+        // Convert to upper case the first character
+        char firstChar = type.charAt(0);
+        String firstletter = String.valueOf(firstChar).toUpperCase();
+
+        // Convert to lower case the rest of the word
+        String restOfWord = type.substring(1).toLowerCase();
+        return firstletter + restOfWord;
+    }
+
+    private EventType eventType;
+}
