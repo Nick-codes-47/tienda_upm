d67e1005373b93cc510a819c5f7792a18240b841
Nico <nicolaslefterache@gmail.com>
2025-11-09 14:06:13 +0100

    handled InvalidProductException if personalizable is less than 1

--- src/main/java/es/upm/etsisi/poo/ProductContainer/CustomProduct.java
+++ src/main/java/es/upm/etsisi/poo/ProductContainer/CustomProduct.java
@@ -2,8 +2,10 @@ package es.upm.etsisi.poo.ProductContainer;
 
 public class CustomProduct extends Product {
 
-    public CustomProduct(int id, String name, double price, String category, int pers) throws InvalidProductException {
+    public CustomProduct(int id, String name, String category, double price, int pers) throws InvalidProductException {
         super(id,name,category,price);
+        if (pers < 1)
+            throw new InvalidProductException("ERROR: A CustomProduct must have at leat one personalizable.");
         this.personalizable = pers;
     }
 
