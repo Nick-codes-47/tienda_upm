44ae25a53ade3b55551b6d8fa570cecf6c3cdf3c
Nico <nicolaslefterache@gmail.com>
2025-11-10 13:10:09 +0100

    added its actions

--- src/main/java/es/upm/etsisi/poo/Requests/CashierHandler.java
+++ src/main/java/es/upm/etsisi/poo/Requests/CashierHandler.java
@@ -1,15 +1,21 @@
 package es.upm.etsisi.poo.Requests;
 
 import es.upm.etsisi.poo.Actions.Action;
+import es.upm.etsisi.poo.Actions.TicketHandlerActions.ListTickets;
+import es.upm.etsisi.poo.Actions.UserHandlerActions.AddUser;
+import es.upm.etsisi.poo.Actions.UserHandlerActions.ListUsers;
+import es.upm.etsisi.poo.Actions.UserHandlerActions.RemoveCashier;
+import es.upm.etsisi.poo.App;
+
+import java.util.HashMap;
 
 public class CashierHandler extends RequestHandler {
-    public CashierHandler() {
+    public CashierHandler(App app) {
         super();
-        // TODO ALL
-    }
-
-    public Action getAction(Request request) {
-        // TODO ALL
-        return null;
+        HashMap<String,Action> actions = super.getActions();
+        actions.put("add", new AddUser(app));
+        actions.put("remove", new RemoveCashier(app));
+        actions.put("list", new ListUsers(app));
+        actions.put("tickets", new ListTickets(app));
     }
 }
