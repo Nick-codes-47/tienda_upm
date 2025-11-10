7d940358298d08c439bff8c0e2a1b326aa4e41ac
Nico <nicolaslefterache@gmail.com>
2025-11-10 13:06:30 +0100

    implemented getAction

--- src/main/java/es/upm/etsisi/poo/Requests/RequestHandler.java
+++ src/main/java/es/upm/etsisi/poo/Requests/RequestHandler.java
@@ -4,7 +4,7 @@ import es.upm.etsisi.poo.Actions.Action;
 
 import java.util.HashMap;
 
-abstract class RequestHandler {
+public class RequestHandler {
     private HashMap<String, Action> actions;
 
     public RequestHandler() {
@@ -15,5 +15,16 @@ abstract class RequestHandler {
         return actions;
     }
 
-    abstract Action getAction(Request request);
+    public Action getAction(Request request) {
+        if (request != null) {
+            // We get the action in the request
+            String action = request.actionId;
+            // We look if we have that action in the map
+            Action actionToReturn = this.actions.get(action);
+            if (actionToReturn == null) {
+                System.err.println("ERROR: No such action: " + action);
+            }
+            return actionToReturn;
+        } else return null;
+    }
 }
