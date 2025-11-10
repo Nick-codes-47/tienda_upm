077bffd0106dd2535cbb03e4839b6dc018f624ca
Nico <nicolaslefterache@gmail.com>
2025-11-10 13:06:14 +0100

    added all its actions

--- src/main/java/es/upm/etsisi/poo/Requests/ProductHandler.java
+++ src/main/java/es/upm/etsisi/poo/Requests/ProductHandler.java
@@ -1,15 +1,27 @@
 package es.upm.etsisi.poo.Requests;
 
 import es.upm.etsisi.poo.Actions.Action;
+import es.upm.etsisi.poo.Actions.ProductHandlerActions.Add.AddEvent;
+import es.upm.etsisi.poo.Actions.ProductHandlerActions.Add.AddProduct;
+import es.upm.etsisi.poo.Actions.ProductHandlerActions.ListProducts;
+import es.upm.etsisi.poo.Actions.ProductHandlerActions.RemoveProduct;
+import es.upm.etsisi.poo.Actions.ProductHandlerActions.UpdateProduct;
+import es.upm.etsisi.poo.App;
+import es.upm.etsisi.poo.ProductContainer.EventType;
+
+import java.util.HashMap;
 
 public class ProductHandler extends RequestHandler{
-    public ProductHandler() {
+    public ProductHandler(App app) {
         super();
-        // TODO ALL
-    }
-
-    public Action getAction(Request request) {
-        // TODO ALL
-        return null;
+        // We obtain the map of actions
+        HashMap<String,Action> actions = super.getActions();
+        // We introduce all the actions that we'll need
+        actions.put("add", new AddProduct(app));
+        actions.put("addFood", new AddEvent(app, EventType.FOOD));
+        actions.put("addMeeting", new AddEvent(app, EventType.MEETING));
+        actions.put("update",new UpdateProduct(app));
+        actions.put("remove", new RemoveProduct(app));
+        actions.put("list", new ListProducts(app));
     }
 }
