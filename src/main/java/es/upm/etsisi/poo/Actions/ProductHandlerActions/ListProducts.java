d5cd8b467d01353ff185d6164f65e3108aa16af2
Nico <nicolaslefterache@gmail.com>
2025-11-10 11:39:08 +0100

    finished class (may need review)

--- src/main/java/es/upm/etsisi/poo/Actions/ProductHandlerActions/ListProducts.java
+++ src/main/java/es/upm/etsisi/poo/Actions/ProductHandlerActions/ListProducts.java
@@ -2,20 +2,51 @@ package es.upm.etsisi.poo.Actions.ProductHandlerActions;
 
 import es.upm.etsisi.poo.Actions.Action;
 import es.upm.etsisi.poo.App;
+import es.upm.etsisi.poo.ProductContainer.BaseProduct;
+
+import java.util.*;
 
 public class ListProducts extends Action {
     public ListProducts(App app) {
         super(app);
     }
 
+    /**
+     * Method that lists the products in ascending order by id
+     * @param args in this action there are no arguments required
+     * @return 0 if it was successful
+     *         3 if they were arguments
+     *         7 if the catalog is empty
+     */
     @Override
     public int execute(String[] args) {
-        // TODO ALL
+        // this action doesn't need arguments (args.length = 0)
+        if (args.length != 0) return 3;
+
+        // We obtain the map of products
+        HashMap<Integer, BaseProduct> products = app.catalog.getProducts();
+        // We check if the catalog is empty
+        if (products.isEmpty()) {
+            System.err.println("ERROR: There are no products in the catalog yet!");
+            return 7;
+        }
+
+        // We list the products in ascending order by their id
+        ArrayList<Map.Entry<Integer,BaseProduct>> entries = new ArrayList<>(products.entrySet());
+        entries.sort(Map.Entry.comparingByKey());
+        System.out.println("Catalog:");
+        for (Map.Entry<Integer, BaseProduct> entry : entries) {
+            // We only show the product
+            System.out.println(" "+entry.getValue());
+        }
         return 0;
     }
 
+    /**
+     * Shows how to call the action to list the products
+     */
     @Override
     public void help() {
-        // TODO ALL
+        System.out.println("prod list");
     }
 }
