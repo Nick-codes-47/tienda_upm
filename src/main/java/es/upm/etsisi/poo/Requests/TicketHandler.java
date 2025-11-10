04037decebb153314e44b5cc196a05d3d791b0e3
Nico <nicolaslefterache@gmail.com>
2025-11-10 13:14:40 +0100

    added its actions

--- src/main/java/es/upm/etsisi/poo/Requests/TicketHandler.java
+++ src/main/java/es/upm/etsisi/poo/Requests/TicketHandler.java
@@ -1,16 +1,21 @@
 package es.upm.etsisi.poo.Requests;
 
 import es.upm.etsisi.poo.Actions.Action;
+import es.upm.etsisi.poo.Actions.TicketHandlerActions.*;
 import es.upm.etsisi.poo.App;
+
+import java.util.HashMap;
 
 public class TicketHandler extends RequestHandler {
     public TicketHandler(App app) {
         super();
-        // TODO ALL
-    }
-
-    public Action getAction(Request request) {
-        // TODO ALL
-        return null;
+        // We obtain the map of actions
+        HashMap<String,Action> actions = super.getActions();
+        // We introduce all the actions that we'll need
+        actions.put("new", new AddTicket(app));
+        actions.put("add", new AddProductToTicket(app));
+        actions.put("remove", new RemoveProductFromTicket(app));
+        actions.put("print",new PrintTicket(app));
+        actions.put("list", new ListTickets(app));
     }
 }
