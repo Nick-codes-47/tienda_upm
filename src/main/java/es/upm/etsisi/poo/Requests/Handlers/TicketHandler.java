0ca111c10237b07c41a67d390ce922e5c83daf60
Alvaro <a.marquez.santamaria@gmail.com>
2025-11-12 16:58:33 +0100

    Ticket Handler edited

--- src/main/java/es/upm/etsisi/poo/Requests/Handlers/TicketHandler.java
+++ src/main/java/es/upm/etsisi/poo/Requests/Handlers/TicketHandler.java
@@ -10,10 +10,8 @@ import java.util.HashMap;
 public class TicketHandler extends RequestHandler {
     public TicketHandler(App app) {
         super();
-        // We obtain the map of actions
         HashMap<String,Action> actions = super.getActions();
-        // We introduce all the actions that we'll need
-        actions.put("new", new AddTicket(app));
+        actions.put("new", new AddNewTicket(app));
         actions.put("add", new AddProductToTicket(app));
         actions.put("remove", new RemoveProductFromTicket(app));
         actions.put("print",new PrintTicket(app));
