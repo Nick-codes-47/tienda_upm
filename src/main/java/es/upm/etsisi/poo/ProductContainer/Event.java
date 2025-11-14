c540b327b7b4c6f1311c92a94e57d855256f87f3
Nico <nicolaslefterache@gmail.com>
2025-11-14 09:48:28 +0100

    added to Event the atribute actualPeople and its set method. Added also the hashcode and equals and modified toString

--- src/main/java/es/upm/etsisi/poo/ProductContainer/Event.java
+++ src/main/java/es/upm/etsisi/poo/ProductContainer/Event.java
@@ -1,6 +1,5 @@
 package es.upm.etsisi.poo.ProductContainer;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 public class Event extends BaseProduct {
@@ -18,18 +17,67 @@ public class Event extends BaseProduct {
     }
 
     public LocalDateTime getExpireDate() { return this.expireDate; }
-    public int getMaxPeople() { return this.maxPeople; }
+
     public EventType getType() { return this.type; }
+
+    /**
+     * Sets the actual people value to the one in the parameter. It checks
+     * @param actualPeople the new value of actualPeople
+     * @throws InvalidProductException Controls that the parameter actualPeople is valid
+     */
+    public void setActualPeople (int actualPeople) throws InvalidProductException {
+        if (actualPeople > maxPeople) {
+            throw new InvalidProductException("ERROR: There can't be more than "+maxPeople+" people");
+        } else if (actualPeople < 1) {
+            throw new InvalidProductException("ERROR: There must be at leat 1 person in the event");
+        }
+        this.actualPeople = actualPeople;
+    }
+
+    /**
+     * Compares this product to another with the ID
+     * @param obj (another)
+     * @return whether the IDs match
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Event other = (Event) obj;
+        return super.getId() == other.getId();
+    }
+
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(super.getId());
+    }
 
     @Override
     public String toString() {
-        return "{class:"+EventType.toSentenceCase(type)+", id:"+super.getId()+", name:'"+super.getName()+
-                ", expiration date:"+this.expireDate.toLocalDate().toString()+", maximum people:"+this.maxPeople+
-                ", price/person:" +super.getPrice()+"}";
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("{class:")
+                .append(EventType.toSentenceCase(type))
+                .append(", id:")
+                .append(super.getId())
+                .append(", name:'")
+                .append(super.getName())
+                .append("', price/person:")
+                .append(super.getPrice())
+                .append(", expiration date:")
+                .append(this.expireDate.toLocalDate().toString())
+                .append(", max people allowed:")
+                .append(this.maxPeople);
+        if (actualPeople != -1)
+            sb.append(", actualPeople:").append(actualPeople);
+
+        return sb.toString();
     }
+
 
     private final LocalDateTime expireDate;
     private final int maxPeople;
+    private int actualPeople = -1;
     private final EventType type;
     private final int MAX_PEOPLE_ALLOWED = 100;
 }
