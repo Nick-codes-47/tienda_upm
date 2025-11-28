88401d29e82e73bedcee4941e6b2dae4a4c209b4
Alvaro <a.marquez.santamaria@gmail.com>
2025-11-28 11:23:36 +0100

    nombre variable errata

--- src/main/java/es/upm/etsisi/poo/Actions/TicketHandlerActions/AddProductToTicket.java
+++ src/main/java/es/upm/etsisi/poo/Actions/TicketHandlerActions/AddProductToTicket.java
@@ -26,9 +26,10 @@ public class AddProductToTicket extends Action {
 
         BaseProduct product;
         try {
-            int productId = Integer.parseInt(amountStr);
+            int productId = Integer.parseInt(prodIdStr);
             product = app.catalog.getProduct(productId);
             if (product == null) {
+                System.out.println("Falla aqui");
                 System.err.printf("ERROR: Product with ID '%s' not found in the Catalog.\n", prodIdStr);
                 return -2;
             }
